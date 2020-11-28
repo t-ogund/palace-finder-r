aba2ed1df3154b50bec0c1607fd9cf284ee2126b
Toye Ogundepo <ogundepo@gmail.com>
2020-11-28 16:45:05 -0600

    edits

--- src/components/HomeBottom.js
+++ src/components/HomeBottom.js
@@ -7,13 +7,13 @@ function HomeBottom() {
         <React.Fragment>
             <Container>
                 <Row className="d-flex justify-content-center">
-                    <Col sm="12" className="home-bottom-header bg-danger">
+                    <Col sm="12" className="home-bottom-header mt-5 mb-5">
                         <h3>With numerous listings and updates, <br/>
                         you're always in the know.
                         </h3>
                     </Col>
                 </Row>
-                <Row className="bg-warning">
+                <Row className="">
                     <Col md="4" className="mt-2 mb-2">
                         <Card body className="tile">
                             <CardTitle tag="h5">Special Title Treatment</CardTitle>
