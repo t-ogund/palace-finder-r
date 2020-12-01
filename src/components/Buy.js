580aca280d2cfde0ed4597c948a0b6f7f64041c8
Toye Ogundepo <ogundepo@gmail.com>
2020-11-30 22:53:20 -0600

    working on results of buy page.

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -4,6 +4,8 @@ import { Row, Col, Container, Button,
      Input, InputGroup, InputGroupAddon } from "reactstrap";
 import PropertyCard from "./PropertyCard";
 import Map from "./Map";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 
 class Buy extends React.Component {
@@ -20,16 +22,16 @@ class Buy extends React.Component {
                 <Container fluid>
                     <Row className="buy-input-buttons pt-4 fixed-top bg-white">
                         <Col xl={2} lg={4} md={6}>
-                            <InputGroup>
+                            <InputGroup className="px-3">
                                 <Input className="buy-input" />
-                                <InputGroupAddon className="buy-add-on" addonType="append"><Button>I'm a button</Button></InputGroupAddon>
+                                <InputGroupAddon className="buy-add-on" addonType="append"><Button className="bg-white text-primary"><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></Button></InputGroupAddon>
                             </InputGroup>
                         </Col>
-                        <Col xl={10} lg={8} md={6} className="">
+                        <Col xl={10} lg={8} md={6} className="px-3">
                             <Button outline color="primary" className="buy-filter-button">For Rent</Button>
                             <Button outline color="primary" className="buy-filter-button">Price</Button>
-                            <Button outline color="primary" className="buy-filter-button">Bed & Bath</Button>
-                            <Button outline color="primary" className="buy-filter-button">Home Type</Button>
+                            <Button outline color="primary" className="buy-filter-button" id="bed-bath">Bed & Bath</Button>
+                            <Button outline color="primary" className="buy-filter-button" id="home-type">Home Type</Button>
                             <Button outline color="primary" className="buy-filter-button">More</Button>
                             <Button color="primary">Save Search</Button>
                         </Col>
@@ -45,13 +47,36 @@ class Buy extends React.Component {
                             <Map />
                         </Col>
                         <Col className="house-hidden-spacer d-none" sm={0} md={8} lg={2}></Col>
-                        <Col className="bg-warning buy-display py-2" id="house-area" sm={{ size: 12, offset: 0}} md={{ size: 12, offset: 0}} lg={{ size: 4, offset: 8}} xl={{ size: 6, offset: 6}}>
-                            <h3>Hello</h3>
-                            <PropertyCard />
+                        <Col className="buy-display py-2" id="house-area" sm={{ size: 12, offset: 0}} md={{ size: 12, offset: 0}} lg={{ size: 4, offset: 8}} xl={{ size: 6, offset: 6}}>
+                            <Row>
+                                <Col>
+                                    <h5>Query Real Estate & Homes for Sale</h5>
+                                </Col>
+                            </Row>
+
+                            <Row>
+                                <Col className="result-subsection d-flex">
+                                    <div className="number-results">
+                                        <p>Number results </p>
+                                    </div>
+                                    <div className="result-filter">
+                                        
+                                    </div>
+                                </Col>
+                            </Row>
+
+                            <Row>
+                                <Col className="" lg={12}>
+                                    <PropertyCard />
+                                    <PropertyCard />
+                                </Col>
+                            </Row>
+                            {/* <h3>Hello</h3> */}
+                            {/* <PropertyCard />
                             <PropertyCard />
                             <PropertyCard />
 
-                            <PropertyCard />
+                            <PropertyCard /> */}
 
                         </Col>
                     </Row>
